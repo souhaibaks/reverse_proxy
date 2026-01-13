3f5af3e81af3a4480d82b2ac8d5073ac7af7658a
Mohamed.AKSIKAS@um6p.ma <Mohamed.AKSIKAS@um6p.ma>
2026-01-13 11:52:23 +0100

    organizing files

---
+++ Backend.go
@@ -0,0 +1,11 @@
+package reverse_proxy
+import (
+	"sync"
+	"net/url"
+)
+type Backend struct {
+	URL          *url.URL `json:"url"`
+	Alive        bool     `json:"alive"`
+	CurrentConns int64    `json:"current_connections"`
+	mux          sync.RWMutex
+}
---
+++ LoadBalancer.go
@@ -0,0 +1,9 @@
+package reverse_proxy
+import (
+	"net/url"
+)
+type LoadBalancer interface {
+	GetNextValidPeer() *Backend
+	AddBackend(backend *Backend)
+	SetBackendStatus(uri *url.URL, alive bool)
+}
